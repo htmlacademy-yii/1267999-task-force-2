b6023bec97e8b5d0065084305551910fdaa39f52
Konstantin <lable_01@mail.ru>
2022-02-19 15:24:38 +0300

    поправки по классу

--- module1-task2/CentralEntity.php
+++ module1-task2/CentralEntity.php
@@ -1,53 +1,76 @@
 <?php
 
-class CentralEntity {
-    const STATUS_NEW = 'new';
-    const STATUS_CANCELED = 'canceled';
-    const STATUS_IN_WORK = 'in work';
-    const STATUS_DONE = 'done';
-    const STATUS_FAILED = 'failed';
-    const ACTION_RESPONSE_TASK = 'response to a task';
-    const COMPLETION_TASK = 'completion of the task';
-    const REFUSAL_TASK = 'refusal of a task';
-    public $idPerformer = '';
-    public $idCustomer = '';
-    private $map = [
-        'status' => [
-            self::STATUS_NEW => 'новое',
-            self::STATUS_CANCELED => 'отменено',
-            self::STATUS_IN_WORK => 'в работе',
-            self::STATUS_DONE => 'выполнено',
-            self::STATUS_FAILED => 'провалено'
-        ],
-        'action' => [
-            self::ACTION_RESPONSE_TASK => 'отклик на задание',
-            self::COMPLETION_TASK => 'завершение задания',
-            self::REFUSAL_TASK => 'отказ от задания'
-        ]
-    ];
-    public function __construct($idPerformer, $idCustomer) {
-        $this->idPerformer = $idPerformer;
-        $this->idCustomer = $idCustomer;
-    }
-    public function returnMap() {
-           return $this->map;
-    }
-    public function transitionStatus($action) {
-        $status = self::STATUS_NEW;
-        if ($action === self::COMPLETION_TASK) {
-            $status = self::STATUS_DONE;
-        } elseif ($action === self::REFUSAL_TASK) {
-            $status = [self::STATUS_NEW, self::STATUS_IN_WORK];
+    class CentralEntity
+    {
+        const STATUS_NEW = 'new';
+        const STATUS_CANCELED = 'canceled';
+        const STATUS_WORK = 'work';
+        const STATUS_DONE = 'done';
+        const STATUS_FAILED = 'failed';
+
+        const ACTION_RESPONSE = 'response';
+        const ACTION_CANCELED = 'canceled';
+        const ACTION_RUN = 'run';
+        const ACTION_REFUSAL = 'refusal';
+        const ACTION_COMPLETION = 'completion';
+
+        public $executorId = '';
+        public $customerId = '';
+
+        private $map = [
+            'status' => [
+                self::STATUS_NEW => 'новое',
+                self::STATUS_CANCELED => 'отменено',
+                self::STATUS_WORK => 'в работе',
+                self::STATUS_DONE => 'выполнено',
+                self::STATUS_FAILED => 'провалено'
+            ],
+            'action' => [
+                self::ACTION_RESPONSE => 'откликнутьcя',
+                self::ACTION_CANCELED => 'отменить',
+                self::ACTION_RUN => 'запустить',
+                self::ACTION_REFUSAL => 'отказаться',
+                self::ACTION_COMPLETION => 'завершить'
+            ]
+        ];
+
+        public function __construct($executorId, $customerId)
+        {
+            $this->executorId = $executorId;
+            $this->customerId = $customerId;
         }
-        return $status;
-    }
-    public function getAvailableActions($status) {
-        $action = '';
-        if ($status === self::STATUS_NEW) {
-            $action = [self::ACTION_RESPONSE_TASK, self::REFUSAL_TASK];
-        } elseif ($status === self::STATUS_IN_WORK) {
-            $action = [self::COMPLETION_TASK, self::REFUSAL_TASK];
+
+        public function returnMap()
+        {
+            return $this->map;
         }
-        return $action;
+
+        public function transitionStatus($action)
+        {
+            $status = self::STATUS_NEW;
+            if ($action === self::ACTION_COMPLETION) {
+                $status = self::STATUS_DONE;
+            } elseif ($action === self::ACTION_REFUSAL) {
+                $status = [self::STATUS_NEW, self::STATUS_WORK];
+            }
+            return $status;
+        }
+
+        public function getAvailableActions($status, $idUser)
+        {
+            $action = '';
+            if ($idUser === $this->executorId) {
+                if ($status === self::STATUS_NEW) {
+                    $action = self::ACTION_RESPONSE;
+                } elseif ($status === self::STATUS_WORK) {
+                    $action = self::ACTION_REFUSAL;
+                }
+            } elseif ($idUser === $this->customerId) {
+                if ($status === self::STATUS_NEW) {
+                    $action = self::ACTION_CANCELED;
+                } elseif ($status === self::STATUS_WORK) {
+                    $action = self::ACTION_COMPLETION;
+                }
+            }
+        }
     }
-}
