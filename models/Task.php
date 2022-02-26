fb34361e7801984fb0057756086f4df8b51d35c0
Konstantin <lable_01@mail.ru>
2022-02-26 16:43:57 +0300

    доработки по замечаниям

---
+++ models/Task.php
@@ -0,0 +1,111 @@
+<?php
+
+    namespace models;
+
+    class Task
+    {
+        const STATUS_NEW = 'new';
+        const STATUS_CANCELED = 'canceled';
+        const STATUS_WORK = 'work';
+        const STATUS_DONE = 'done';
+        const STATUS_FAILED = 'failed';
+
+        const ACTION_RESPONSE = 'response';
+        const ACTION_CANCELED = 'canceled';
+        const ACTION_RUN = 'run';
+        const ACTION_REFUSAL = 'refusal';
+        const ACTION_COMPLETION = 'completion';
+
+        public $executorId = null;
+        public $customerId = null;
+
+        private $map = [
+            'status' => [
+                self::STATUS_NEW => 'новое',
+                self::STATUS_CANCELED => 'отменено',
+                self::STATUS_WORK => 'в работе',
+                self::STATUS_DONE => 'выполнено',
+                self::STATUS_FAILED => 'провалено'
+            ],
+            'action' => [
+                self::ACTION_RESPONSE => 'откликнутьcя',
+                self::ACTION_CANCELED => 'отменить',
+                self::ACTION_RUN => 'запустить',
+                self::ACTION_REFUSAL => 'отказаться',
+                self::ACTION_COMPLETION => 'завершить'
+            ]
+        ];
+
+        public function __construct($executorId, $customerId)
+        {
+            $this->executorId = $executorId;
+            $this->customerId = $customerId;
+        }
+
+        public function returnMap()
+        {
+            return $this->map;
+        }
+
+        public function transitionStatus($action)
+        {
+            switch ($action) {
+                case self::ACTION_COMPLETION:
+                    return self::STATUS_DONE;
+                    break;
+                case self::ACTION_CANCELED:
+                    return self::STATUS_CANCELED;
+                    break;
+                case self::ACTION_REFUSAL:
+                    return self::STATUS_FAILED;
+                    break;
+                default:
+                    return null;
+            }
+        }
+
+        public function getAvailableActions($status, $userId)
+        {
+            switch ($status) {
+                case self::STATUS_NEW:
+                    if ($userId === $this->executorId) {
+                        return self::ACTION_RESPONSE;
+                    } elseif ($userId === $this->customerId) {
+                        return [self::ACTION_RUN, self::ACTION_CANCELED];
+                    }
+                    break;
+                case self::STATUS_WORK:
+                    if ($userId === $this->executorId) {
+                        return self::ACTION_REFUSAL;
+                    } elseif ($userId === $this->customerId) {
+                        return self::ACTION_COMPLETION;
+                    }
+                    break;
+                default:
+                    return null;
+            }
+        }
+
+
+//        public function getAvailableActions($status, $userId)
+//        {
+//            $action = '';
+//            if ($userId === $this->executorId) {
+//                if ($status === self::STATUS_NEW) {
+//                    $action = self::ACTION_RESPONSE;
+//                } elseif ($status === self::STATUS_WORK) {
+//                    $action = self::ACTION_REFUSAL;
+//                }
+//            } elseif ($userId === $this->customerId) {
+//                if ($status === self::STATUS_NEW) {
+//                    $action = [self::ACTION_RUN, self::ACTION_CANCELED];
+//                } elseif ($status === self::STATUS_WORK) {
+//                    $action = self::ACTION_COMPLETION;
+//                }
+//            }
+//            return $action;
+//        }
+    }
+
+
+
--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -1,33 +1,36 @@
 <<?php
-    class CentralEntityTest extends \PHPUnit\Framework\TestCase {
+
+    /*
+     * vendor/bin/phpunit --testdox / запуск теста
+     */
+    class taskTest extends \PHPUnit\Framework\TestCase {
         public function testTransitionStatus () {
             $executorId = 100;
             $customerId = 50;
-            $centralEntity = new Class\CentralEntity($executorId, $customerId);
+            $centralEntity = new models\Task($executorId, $customerId);
             $resultResponse = $centralEntity->transitionStatus($centralEntity::ACTION_RESPONSE);
             $resultCanceled = $centralEntity->transitionStatus($centralEntity::ACTION_CANCELED);
             $resultRun = $centralEntity->transitionStatus($centralEntity::ACTION_RUN);
             $resultRefusal = $centralEntity->transitionStatus($centralEntity::ACTION_REFUSAL);
             $resultCompletion = $centralEntity->transitionStatus($centralEntity::ACTION_COMPLETION);
-            $this->assertEquals($centralEntity::STATUS_NEW, $resultResponse);
-            $this->assertEquals($centralEntity::STATUS_NEW, $resultCanceled);
-            $this->assertEquals($centralEntity::STATUS_NEW, $resultRun);
-            $this->assertEquals([$centralEntity::STATUS_NEW, $centralEntity::STATUS_WORK], $resultRefusal);
+            $this->assertEquals(null, $resultResponse);
+            $this->assertEquals($centralEntity::STATUS_CANCELED, $resultCanceled);
+            $this->assertEquals(null, $resultRun);
+            $this->assertEquals($centralEntity::STATUS_FAILED, $resultRefusal);
             $this->assertEquals($centralEntity::STATUS_DONE, $resultCompletion);
         }
 
         public function testGetAvailableActions () {
             $executorId = 100;
             $customerId = 50;
-            $centralEntity = new Class\CentralEntity($executorId, $customerId);
-
+            $centralEntity = new models\Task($executorId, $customerId);
             $resultNew = $centralEntity->getAvailableActions($centralEntity::STATUS_NEW, $centralEntity->executorId);
             $resultWork = $centralEntity->getAvailableActions($centralEntity::STATUS_WORK, $centralEntity->executorId);
             $resultNewCustomer = $centralEntity->getAvailableActions($centralEntity::STATUS_NEW, $centralEntity->customerId);
             $resultWorkCustomer = $centralEntity->getAvailableActions($centralEntity::STATUS_WORK, $centralEntity->customerId);
             $this->assertEquals($centralEntity::ACTION_RESPONSE, $resultNew);
             $this->assertEquals($centralEntity::ACTION_REFUSAL, $resultWork);
-            $this->assertEquals($centralEntity::ACTION_CANCELED, $resultNewCustomer);
+            $this->assertEquals([$centralEntity::ACTION_RUN,$centralEntity::ACTION_CANCELED], $resultNewCustomer);
             $this->assertEquals($centralEntity::ACTION_COMPLETION, $resultWorkCustomer);
         }
     }
