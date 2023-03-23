c9d7fc9b0cc807c92587932341c4c22bb48625f0
Konstanin <lable_01@mail.ru>
2023-03-24 00:31:02 +0300

    generate fixtures

--- config/console.php
+++ config/console.php
@@ -28,13 +28,14 @@ $config = [
         'db' => $db,
     ],
     'params' => $params,
-    /*
     'controllerMap' => [
-        'fixture' => [ // Fixture generation command line.
+        'fixtures' => [
             'class' => 'yii\faker\FixtureController',
+            'templatePath' => '@app/common/fixtures/templates',
+            'fixtureDataPath' => '@app/common/fixtures/data',
+            'namespace' => 'common\fixtures',
         ],
     ],
-    */
 ];
 
 if (YII_ENV_DEV) {
